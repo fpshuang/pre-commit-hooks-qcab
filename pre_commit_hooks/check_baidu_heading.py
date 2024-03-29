849d27608b648f0d7195d114369c4dc3b07a9336
huangpengsheng <huangpengsheng@baidu.com>
2024-03-29 14:23:21 +0800

    add baidu heading

---
+++ pre_commit_hooks/check_baidu_heading.py
@@ -0,0 +1,149 @@
+import argparse
+import os
+import os.path as osp
+from typing import List
+
+HEADER = 'Author: Netdisk-AI team.'
+
+HEADER_KEYWORDS = {'Author', 'Netdisk-AI', 'team'}
+
+
+def has_heading(lines: List[str]) -> bool:
+    for line in lines:
+        if not HEADER_KEYWORDS.isdisjoint(set(line.split(' '))):
+            return True
+    return False
+
+
+def parse_args():
+    parser = argparse.ArgumentParser(description='Add heading to files')
+    parser.add_argument(
+        'includes', type=str, nargs='+', help='directory to add heading')
+    parser.add_argument(
+        '--excludes',
+        nargs='*',
+        type=str,
+        default=[],
+        help='excludes directory')
+    parser.add_argument(
+        '--suffixes',
+        nargs='*',
+        type=str,
+        default=['.py'],
+        help='heading will be added to files with suffixes')
+    parser.add_argument('--ignore-file-not-found-error', action='store_true')
+    args = parser.parse_args()
+    return args
+
+
+def check_args(includes: List[str],
+               excludes: List[str],
+               suffixes: List[str],
+               ignore_file_not_found_error: bool = False):
+    """Check the correctness of args and format them."""
+
+    valid_suffixes = {'.py', '.h', '.cpp', '.cu', '.cuh', '.hpp'}
+
+    # remove possible duplication
+    includes = list(set(includes))
+    excludes = list(set(excludes))
+    suffixes = list(set(suffixes))
+
+    # check the correctness and format args
+    for i, dir in enumerate(includes):
+        if not osp.exists(dir):
+            if not ignore_file_not_found_error:
+                raise FileNotFoundError(f'{dir} can not be found')
+        else:
+            includes[i] = osp.abspath(dir)
+
+    for i, dir in enumerate(excludes):
+        if not osp.exists(dir):
+            if not ignore_file_not_found_error:
+                raise FileNotFoundError(f'{dir} can not be found')
+        else:
+            excludes[i] = osp.abspath(dir)
+
+    for suffix in suffixes:
+        if suffix not in valid_suffixes:
+            raise ValueError(
+                f'Expected suffixes are {valid_suffixes}, but got {suffix}')
+
+    return includes, excludes, suffixes
+
+
+def get_filepaths(includes: List[str], excludes: List[str],
+                  suffixes: List[str]) -> List[str]:
+    """Get all file paths that match the args."""
+
+    filepaths = []
+    for include in includes:
+        for root, _, files in os.walk(include):
+            is_exclude = False
+            for exclude in excludes:
+                if root.startswith(exclude):
+                    is_exclude = True
+                    break
+            if is_exclude:
+                continue
+            else:
+                for file in files:
+                    _, ext = osp.splitext(file)
+                    if ext in suffixes:
+                        filepath = osp.join(root, file)
+                        filepaths.append(filepath)
+    return filepaths
+
+
+def check_heading(includes: List[str],
+                    excludes: List[str],
+                    suffixes: List[str],
+                    ignore_file_not_found_error: bool = False) -> int:
+    """Add heading for those files which lack heading.
+
+    Args:
+        includes: Directory to add heading.
+        excludes: Exclude directory.
+        suffixes: heading will be added to files with suffixes.
+        ignore_file_not_found_error: Whether to ignore `FileNotFoundError` when
+            some directories are specified to add heading but they are not
+            found.
+
+    returns:
+        Returns 0 if no file is missing heading, otherwise returns 1.
+    """
+    rev = 0
+    fixed_filepaths = []
+    try:
+        includes, excludes, suffixes = check_args(includes, excludes, suffixes,
+                                                  ignore_file_not_found_error)
+    except (FileNotFoundError, ValueError) as e:
+        print(repr(e))
+        return 1
+    else:
+        filepaths = get_filepaths(includes, excludes, suffixes)
+        for filepath in filepaths:
+            with open(filepath, encoding='utf-8') as f:
+                lines = f.readlines()
+            if not has_heading(lines):
+                fixed_filepaths.append(filepath)
+                with open(filepath, 'w', encoding='utf-8') as f:
+                    prefix = '"""' if osp.splitext(
+                        filepath)[1] == '.py' else '// '
+                    suffix = '\n"""\n' if osp.splitext(
+                        filepath)[1] == '.py' else '\n'
+                    f.writelines([prefix + HEADER + suffix] + lines)
+                    rev = 1
+    for filepath in fixed_filepaths:
+        print(f'Fixed {filepath}')
+    return rev
+
+
+def main():
+    args = parse_args()
+    return check_heading(args.includes, args.excludes, args.suffixes,
+                           args.ignore_file_not_found_error)
+
+
+if __name__ == '__main__':
+    raise SystemExit(main())
